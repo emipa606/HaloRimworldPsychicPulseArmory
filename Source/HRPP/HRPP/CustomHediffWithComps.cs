29e0c40c5628efb9c4aa2fa6dc6f1d0938613b49
Kik0hi <balderacchi.kylian@laposte.net>
2020-01-12 11:03:02 +0100

    Rifle and base mod

---
+++ Source/HRPP/HRPP/CustomHediffWithComps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace HRPP
+{
+    class CustomHediffWithComps : HediffWithComps
+    {
+        public override void Tick()
+        {
+            base.Tick();
+            this.severityInt -= 0.00002f;
+            if (this.severityInt > 0.8)
+            {
+                this.pawn.health.AddHediff(HediffDefOf.HRPP_downed);
+            }
+            else if(this.pawn.health.hediffSet.hediffs.Where((Hediff h) => h.def.defName == "HRPP_downed") is Hediff hediff && hediff != null)
+            {
+                this.pawn.health.RemoveHediff(hediff);
+            }
+        }
+    }
+}
---
+++ Source/HRPP/HRPP/HediffDefOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace HRPP
+{
+    [DefOf]
+    public static class HediffDefOf
+    {
+        public static HediffDef HRPP_downed;
+    }
+}
